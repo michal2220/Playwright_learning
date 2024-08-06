b89ce43f4ad2c60486423583be887bbc4da4cea5
Michal G <gawlas05@gmail.com>
2024-08-06 14:30:46 +0200

    Adding cookies

--- api-calls/getLoginToken.js
+++ api-calls/getLoginToken.js
@@ -3,9 +3,8 @@ import * as nodeFetch from "node-fetch"
 export const getLoginToken = async () => {
     const response = await nodeFetch("http://localhost:2221/api/login", {
         method: "POST",
-        body: {
-            "username": "admin",
-            "password": "Admin123"
-          }
-})
+        body: JSON.stringify({ "username": "admin", "password": "Admin123" })
+    })
+    const body = await response.json()
+    return body.token
 }
