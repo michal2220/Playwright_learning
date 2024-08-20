3032bc0043987c2e3750b2192344e921221d9ff2
Michal G <gawlas05@gmail.com>
2024-08-20 13:24:32 +0200

    Mocking network requests

--- tests/my_account.spec.js
+++ tests/my_account.spec.js
@@ -3,8 +3,17 @@ import { myAccountPage } from "../page-objects/MyAccountPage"
 import { getLoginToken } from "../api-calls/getLoginToken"
 import { adminDetails } from "../data/userDetails"
 
-test.only("My account using cookie injection", async ({ page }) => {
+test.only("My account using cookie injection and mocking network request", async ({ page }) => {
     const loginToken = await getLoginToken(adminDetails.username, adminDetails.password)
+
+    await page.route("**/api/user**", async (route, request) => {
+        await route.fulfill({
+            status: 500,
+            contentType: "application/json",
+            body: JSON.stringify({ message: "PLAYWRIGHT ERROR FOR MOCKING" })
+        })
+    })
+
     const myAccount = new myAccountPage(page)
     await myAccount.visit()
     await page.evaluate(([loginTokenInsideBrowserCode]) => {
@@ -13,4 +22,5 @@ test.only("My account using cookie injection", async ({ page }) => {
 
     await myAccount.visit()
     await myAccount.waitForPageHeading()
+    await myAccount.waitForErrorMessage()
 })
